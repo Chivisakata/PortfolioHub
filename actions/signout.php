31110bb1999cbbc6effe3f3b9ed94556a84f617b
Vĩ Cao <vifreefire@gmail.com>
2026-06-23 22:39:55 +0700

    feat: add sign out fea

---
+++ actions/signout.php
@@ -0,0 +1,11 @@
+<?php
+session_start();
+if(isset($_SESSION["userId"])){
+    session_destroy();
+    header("Location: ../index.php");
+    exit();
+} else {
+    header("Location: ../index.php");
+    exit();
+}
+?>
