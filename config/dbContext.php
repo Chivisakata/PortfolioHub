69435df8bdd6ae0f691624e272779706de211e7e
Vĩ Cao <vifreefire@gmail.com>
2026-06-27 13:13:34 +0700

    fix: fix can not create session

--- config/dbContext.php
+++ config/dbContext.php
@@ -5,6 +5,7 @@ $password = "";
 $database = "portfoliohub";
 
 $conn = mysqli_connect($host, $user, $password, $database);
+$conn->set_charset("utf8mb4");
 
 if (!$conn) {
     die("Kết nối thất bại: " . mysqli_connect_error());
